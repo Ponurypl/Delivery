a10687824e27cdf99f263523c4f2cc3c267adbb5
Paweł Nowak <666ponury@gmail.com>
2022-09-25 21:42:38 +0200

    Dodanie solucji i pierwszej encji

---
+++ src/Delivery.Domain/Transpot/Entities/UniqueUnit.cs
@@ -0,0 +1,8 @@
+﻿namespace Delivery.Domain.Transpot.Entities;
+
+public class UniqueUnit
+{
+    public int Id { get; set; }
+    public int TransportUnitId { get; set; }
+    public string Barcode { get; set; } = default!;
+}
