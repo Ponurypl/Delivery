6138eecf6edf61ba435a9b321c586f183b58f7d0
Paweł Nowak <666ponury@gmail.com>
2022-09-30 21:34:00 +0200

    Pierwszy command i command handler

---
+++ src/Application/Deliveries/Command/CreateNewTransport/CreateNewTransportCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace MultiProject.Delivery.Application.Deliveries.Command.CreateNewTransport;
+
+public sealed record CreateNewTransportCommand : IRequest
+{
+    public string Number { get; set; }
+}
---
+++ src/Application/Deliveries/Command/CreateNewTransport/CreateNewTransportCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace MultiProject.Delivery.Application.Deliveries.Command.CreateNewTransport;
+
+public class CreateNewTransportCommandHandler : IRequestHandler<CreateNewTransportCommand>
+{
+    public Task<Unit> Handle(CreateNewTransportCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
