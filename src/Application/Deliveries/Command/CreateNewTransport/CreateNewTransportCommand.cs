0146e88c1dff978837e9d88bf161d64c540835bb
Paweł Nowak <666ponury@gmail.com>
2022-10-02 15:34:03 +0200

    Usunięcie na razie zbędnego przykładowego handlera

--- src/Application/Deliveries/Command/CreateNewTransport/CreateNewTransportCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace MultiProject.Delivery.Application.Deliveries.Command.CreateNewTransport;
-
-public sealed record CreateNewTransportCommand : IRequest
-{
-    public string Number { get; set; }
-}
