68b9a5b6638859461d594338cc671d9450025af2
Łukasz Burchard <llukkassi@gmail.com>
2022-10-10 20:18:09 +0200

    Zalążek aktualizacji geolokalizacji uzytkownika
    Dodanie command, command handlera

---
+++ src/Application/Users/Commands/UpdateUserAdvancedLocalization/UpdateUserAdvancedLocalizationHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using MultiProject.Delivery.Application.Common.Interfaces.Repositories;
+using MultiProject.Delivery.Domain.Common.Entities;
+using MultiProject.Delivery.Domain.Users.Entities;
+using MultiProject.Delivery.Domain.Users.Exceptions;
+
+namespace MultiProject.Delivery.Application.Users.Commands.UpdateUserAdvancedLocalization;
+
+public sealed class UpdateUserAdvancedLocalizationHandler : IHandler<UpdateUserAdvancedLocalizationCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUserRepository _userRepository;
+    private readonly IDateTime _dateTime;
+
+    public UpdateUserAdvancedLocalizationHandler(IUnitOfWork unitOfWork, IUserRepository userRepository, IDateTime dateTime)
+    {
+        _unitOfWork = unitOfWork;
+        _userRepository = userRepository;
+        _dateTime = dateTime;
+    }
+
+    public async Task<Unit> Handle(UpdateUserAdvancedLocalizationCommand request, CancellationToken cancellationToken)
+    {
+        AdvancedGeolocalization advancedGeolocalization = new()
+        {
+            Latitude = request.Latitude,
+            Longitude = request.Longitude,
+            Accuracy = request.Accuracy,
+            GeoHeading = request.GeoHeading,
+            GeoReadDate = _dateTime.Now,
+            GeoSpeed = request.GeoSpeed
+        };
+
+        User user = await _userRepository.GetByIdAsync(request.UserId);
+        if (user is null) throw new UserNotFoundException(nameof(request.UserId));
+
+        user.Geolocalization = advancedGeolocalization;
+        _userRepository.Update(user);
+        await _unitOfWork.SaveChangesAsync();
+
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Application/Users/Commands/UpdateUserAdvancedLocalization/UpdateUserLocalizationCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiProject.Delivery.Application.Users.Commands.UpdateUserAdvancedLocalization;
+
+public sealed record UpdateUserAdvancedLocalizationCommand : ICommand
+{
+    public Guid UserId { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Accuracy { get; set; }
+    public double? GeoHeading { get; set; }
+    public double? GeoSpeed { get; set; }
+}
