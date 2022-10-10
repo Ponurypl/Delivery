6b3ae744e4beeef5661beb68e974c596fe6c794c
Łukasz Burchard <llukkassi@gmail.com>
2022-10-10 20:18:48 +0200

    pierwszy zalążek dodawania skanów
    dodanie baardzo wstępnego scan command

---
+++ src/Application/Delivieries/CreateScan/CreateScanCommand.cs
@@ -0,0 +1,11 @@
+﻿using MultiProject.Delivery.Domain.Common.Entities;
+
+namespace MultiProject.Delivery.Application.Delivieries.CreateScan;
+
+public sealed record CreateScanCommand : ICommand
+{
+    public int TransportUnitId { get; set; }
+    public double? Quanitity { get; set; }
+    public Guid DelivererId { get; set; }
+    public Geolocation? Geolocalization { get; set; }
+}
