d49cc9a9218a574e6bc6607ecd7d20eceba4017d
Łukasz Burchard <llukkassi@gmail.com>
2022-10-11 19:07:40 +0200

    Wyrzucenie zbędnych repozytoriów
    Wyrzucone repozytoria unique i multi unit details, aktualnie są niepotrzebne,
    a i tak przerabiać będziemy transport na agreagate root

--- src/Application/Common/Interfaces/Repositories/ITransportMultiUnitDetalisRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MultiProject.Delivery.Domain.Deliveries.Entities;
-
-namespace MultiProject.Delivery.Application.Common.Interfaces.Repositories;
-
-public interface ITransportMultiUnitDetalisRepository
-{
-    void Add(MultiUnitDetails multiUnitDetails);
-    Task<MultiUnitDetails> GetByIdAsync(int id);
-}
--- src/Application/Common/Interfaces/Repositories/ITransportUniqueUnitDetalisRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MultiProject.Delivery.Domain.Deliveries.Entities;
-
-namespace MultiProject.Delivery.Application.Common.Interfaces.Repositories;
-
-public interface ITransportUniqueUnitDetalisRepository
-{
-    void Add(UniqueUnitDetails uniqueUnitDetails);
-    Task<UniqueUnitDetails> GetByIdAsync(int id);
-}
