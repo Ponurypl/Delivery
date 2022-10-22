cd49144b4c234336b9d0b5443ab55fdaee6d9f20
Łukasz Burchard <llukkassi@gmail.com>
2022-10-22 18:05:58 +0200

    Dodanie Transport i TransportUnit Validatora

---
+++ src/Domain/Deliveries/Validators/TransportUnitValidator.cs
@@ -0,0 +1,31 @@
+﻿using MultiProject.Delivery.Domain.Deliveries.ValueTypes;
+using MultiProject.Delivery.Domain.Common.Extensions;
+
+namespace MultiProject.Delivery.Domain.Deliveries.Validators
+{
+    internal class TransportUnitValidator : AbstractValidator<NewTransportUnit>
+    {
+        public TransportUnitValidator()
+        {
+            //TODO: Aktualnie może sypnąć tym samym błędem X razy, a alternatywa jest... nieczytelna
+            RuleFor(x => x).Must(x => string.IsNullOrWhiteSpace(x.Barcode) && (x.Amount is null || x.UnitOfMeasureId is null || x.Amount == 0 || x.UnitOfMeasureId == 0))
+                           .WithMessage($"Transport unit not specified properly. You should specify Barcode or UnitOfMeasure together with Amount");
+            RuleFor(x => x).Must(x => !string.IsNullOrWhiteSpace(x.Barcode) && (x.Amount is not null || x.UnitOfMeasureId is not null))
+                           .WithMessage($"Transport unit not specified properly. You should specify Barcode or UnitOfMeasure together with Amount");
+            RuleFor(x => x.Amount).NotEmpty().When(x => x.UnitOfMeasureId is null)
+                           .WithMessage($"Transport unit not specified properly. You should specify Barcode or UnitOfMeasure together with Amount");
+            RuleFor(x => x.UnitOfMeasureId).NotEmpty().When(x => x.UnitOfMeasureId is null)
+                           .WithMessage($"Transport unit not specified properly. You should specify Barcode or UnitOfMeasure together with Amount");
+
+            /* Alternatywa?
+
+            RuleFor(x => x).Must(x => (string.IsNullOrWhiteSpace(x.Barcode) 
+                                       && (x.Amount is null || x.UnitOfMeasureId is null || x.Amount == 0 || x.UnitOfMeasureId == 0))
+                                  && (!string.IsNullOrWhiteSpace(x.Barcode) && (x.Amount is not null || x.UnitOfMeasureId is not null))
+                                  && ((x.Amount != 0 && x.Amount is not null) || (x.UnitOfMeasureId is not null && x.UnitOfMeasureId != 0)))
+                           .WithMessage($"Transport unit not specified properly. You should specify Barcode or UnitOfMeasure together with Amount");
+            */
+
+        }
+    }
+}
---
+++ src/Domain/Deliveries/Validators/TransportValidator.cs
@@ -0,0 +1,16 @@
+﻿using MultiProject.Delivery.Domain.Deliveries.Entities;
+
+namespace MultiProject.Delivery.Domain.Deliveries.Validators;
+
+public sealed class TransportValidator : AbstractValidator<Transport>
+{
+    public TransportValidator()
+    {
+        RuleFor(x => x.Deliverer).NotEmpty();
+        RuleFor(x => x.Deliverer.Role).Equal(Users.Enums.UserRole.Deliverer);
+        RuleFor(x => x.Manager).NotEmpty();
+        RuleFor(x => x.Manager.Role).Equal(Users.Enums.UserRole.Manager);
+        RuleFor(x => x.TransportUnits).NotEmpty();
+
+    }
+}
