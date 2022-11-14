c0bc06925a35a7ac49285f986f65506c5558870d
Łukasz Burchard <llukkassi@gmail.com>
2022-11-14 16:09:36 +0100

    Dodanie GeolocationToaAddDto w aplikacji zamiast geolokacji z domeny
    dorzucenie komentarza apropo weryfikacji dodwania ilości do skanu gdy ten jest typu unique/barcode

--- src/Application/Deliveries/Commands/CreateScan/CreateScanCommand.cs
+++ src/Application/Deliveries/Commands/CreateScan/CreateScanCommand.cs
@@ -1,5 +1,4 @@
 ﻿using MultiProject.Delivery.Application.Delivieries.CreateScan;
-using MultiProject.Delivery.Domain.Common.ValueTypes;
 
 namespace MultiProject.Delivery.Application.Deliveries.Commands.CreateScan;
 
@@ -10,5 +9,7 @@ public sealed record CreateScanCommand : ICommand<ScanCreatedDto>
     public Guid DelivererId { get; init; }
 
     //TODO: Obiekt Geolocation nie ma prawa istnieć w modelu przychodzącym z zewnątrz. Stwórz analogiczny tutaj i przepisz do prawdziwego Geolocation.
-    public Geolocation? Location { get; init; }
+    // czy tu nie powinniśmy ogólnie zrobić "newScanToCreateDto", w którym byłyby te wszystkie pola podane w "płaski sposób"? coś na stylu "TransportUnitToCreate" w aplikacji i "NewTransportUnit" w domenie, a przepisywać w sumie nie mam za bardzo co chyba skoro w handlerze jest po odpalana metoda dodaj geolokację, a nie przekazywany model?
+    // stworzyłęm kopię, ale nie wiem czy to dobrze, bo w domenie jest tam np. metoda "create" w kopii nie ma
+    public GeolocationToAddDto? Location { get; init; }
 }
---
+++ src/Application/Deliveries/Commands/CreateScan/GeolocationToAddDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MultiProject.Delivery.Application.Deliveries.Commands.CreateScan;
+
+public sealed record GeolocationToAddDto
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Accuracy { get; set; }
+}
