8ac7672febcc1e166019f70b3a08c786d7a8177d
Łukasz Burchard <llukkassi@gmail.com>
2022-12-01 17:59:44 +0100

    Wydzielenie walidatora TransportUnit z walidatora Transportu

--- src/Application/Deliveries/Commands/CreateTransport/CreateTransportCommandValidator.cs
+++ src/Application/Deliveries/Commands/CreateTransport/CreateTransportCommandValidator.cs
@@ -12,48 +12,8 @@ public sealed class CreateTransportCommandValidator : AbstractValidator<CreateTr
         RuleFor(x => x.Number).NotEmpty();
 
         RuleFor(x => x.TransportUnits).NotEmpty();
-        //TODO: Wydzielić do osobnego validatora
-        RuleForEach(x => x.TransportUnits).ChildRules(x => 
-        { 
-            x.RuleFor(x => x)
-             .Must(x => (BarcodeNotEmpty(x) || MultiUnitFieldsNotEmpty(x)) &&
-                        (!BarcodeNotEmpty(x) || !MultiUnitFieldsNotEmpty(x)))
-             .WithMessage("Treść do zwrócenia w ryj");
-
-            //x.RuleFor(x => x.Barcode).NotEmpty().When(x => x.Amount is null || x.UnitOfMeasureId is null);
-
-            x.RuleFor(x => x.Amount).GreaterThan(0).When(MultiUnitFieldsNotEmpty);
-            //x.RuleFor(x => x.Amount).GreaterThan(0).When(x => x.UnitOfMeasureId is not null && string.IsNullOrWhiteSpace(x.Barcode));
-            //x.RuleFor(x => x.Amount).Empty().When(x => x.Barcode is not null);
-
-            //x.RuleFor(x => x.UnitOfMeasureId).NotEmpty().When(x => x.Amount is not null && string.IsNullOrWhiteSpace(x.Barcode));
-            //x.RuleFor(x => x.UnitOfMeasureId).Empty().When(x => x.Barcode is not null);
-
-            x.RuleFor(x => x.Description).NotEmpty();
-            x.RuleFor(x => x.Number).NotEmpty();
-            x.RuleFor(x => x.RecipientPhoneNumber).NotEmpty();
-            x.RuleFor(x => x.RecipientStreetNumber).NotEmpty();
-            x.RuleFor(x => x.RecipientTown).NotEmpty();
-            x.RuleFor(x => x.RecipientCountry).NotEmpty();
-            x.RuleFor(x => x.RecipientPostCode).NotEmpty();
-
-            x.RuleFor(x => x.RecipientName).NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.RecipientLastName));
-            x.RuleFor(x => x.RecipientLastName).NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.RecipientName));
-            x.RuleFor(x => x.RecipientCompanyName).NotEmpty().When(x => string.IsNullOrWhiteSpace(x.RecipientLastName) 
-                                                                            || string.IsNullOrWhiteSpace(x.RecipientName));
-                
-            });
+        RuleForEach(x => x.TransportUnits).SetValidator(new TransportUnitToCreateValidator());
 
         RuleFor(x => x.TotalWeight).GreaterThan(0).When(x => x.TotalWeight is not null);
-    }
-
-    private static bool MultiUnitFieldsNotEmpty(TransportUnitToCreate x)
-    {
-        return x.Amount is not null && x.UnitOfMeasureId is not null;
-    }
-
-    private static bool BarcodeNotEmpty(TransportUnitToCreate x)
-    {
-        return !string.IsNullOrWhiteSpace(x.Barcode);
     }
 }
---
+++ src/Application/Deliveries/Commands/CreateTransport/TransportUnitToCreateValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+
+namespace MultiProject.Delivery.Application.Deliveries.Commands.CreateTransport;
+public sealed class TransportUnitToCreateValidator : AbstractValidator<TransportUnitToCreate>
+{
+    public TransportUnitToCreateValidator()
+    {
+        RuleFor(x => x)
+             .Must(x => AtLeastOneTypeOfDetailsIsGiven(x) && OnlyOneTypeOfDetailsIsGiven(x))
+             .WithMessage("One Type of TransportUnitDetails need to be given. MultiUnitDetail(UnitOfMeasureId + Amount) OR UniqueUnitDetail(Barcode).");
+
+        RuleFor(x => x.Amount).GreaterThan(0).When(MultiUnitFieldsNotEmpty);
+        RuleFor(x => x.Description).NotEmpty();
+        RuleFor(x => x.Number).NotEmpty();
+        RuleFor(x => x.RecipientPhoneNumber).NotEmpty();
+        RuleFor(x => x.RecipientStreetNumber).NotEmpty();
+        RuleFor(x => x.RecipientTown).NotEmpty();
+        RuleFor(x => x.RecipientCountry).NotEmpty();
+        RuleFor(x => x.RecipientPostCode).NotEmpty();
+
+        RuleFor(x => x.RecipientName).NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.RecipientLastName));
+        RuleFor(x => x.RecipientLastName).NotEmpty().Unless(x => string.IsNullOrWhiteSpace(x.RecipientName));
+        RuleFor(x => x.RecipientCompanyName).NotEmpty().When(x => string.IsNullOrWhiteSpace(x.RecipientLastName)
+                                                                        || string.IsNullOrWhiteSpace(x.RecipientName));
+    }
+    private static bool AtLeastOneTypeOfDetailsIsGiven(TransportUnitToCreate x)
+    {
+        return UniqueUnitFieldsNotEmpty(x) || MultiUnitFieldsNotEmpty(x);
+    }
+
+    private static bool OnlyOneTypeOfDetailsIsGiven(TransportUnitToCreate x)
+    {
+        return !UniqueUnitFieldsNotEmpty(x) || !MultiUnitFieldsNotEmpty(x);
+    }
+
+    private static bool MultiUnitFieldsNotEmpty(TransportUnitToCreate x)
+    {
+        return x.Amount is not null && x.UnitOfMeasureId is not null;
+    }
+
+    private static bool UniqueUnitFieldsNotEmpty(TransportUnitToCreate x)
+    {
+        return !string.IsNullOrWhiteSpace(x.Barcode);
+    }
+}
