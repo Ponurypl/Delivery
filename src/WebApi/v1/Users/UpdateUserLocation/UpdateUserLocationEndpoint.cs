e201c73f80d9d80d2036cb4ac1068ddf379faad8
Łukasz Burchard <llukkassi@gmail.com>
2022-12-16 18:09:48 +0100

    Dodanie Endpointa UpdateUserLocation

---
+++ src/WebApi/v1/Users/UpdateUserLocation/UpdateUserLocationEndpoint.cs
@@ -0,0 +1,46 @@
+﻿using ErrorOr;
+using MediatR;
+using MultiProject.Delivery.Application.Users.Commands.UpdateUserLocation;
+
+namespace WebApi.v1.Users.UpdateUserLocation;
+
+public class UpdateUserLocationEndpoint : Endpoint<UpdateUserLocationRequest>
+{
+    private readonly ISender _sender;
+
+    public UpdateUserLocationEndpoint(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public override void Configure()
+    {
+        Put("users");
+        Version(1);
+    }
+
+    public override async Task HandleAsync(UpdateUserLocationRequest req, CancellationToken ct)
+    {
+        ErrorOr<Success> response = await _sender.Send(new UpdateUserLocationCommand() 
+                                                      { 
+                                                        UserId = req.UserId,
+                                                        Latitude = req.Latitude,
+                                                        Longitude = req.Longitude,
+                                                        Accuracy = req.Accuracy,
+                                                        Heading = req.Heading,
+                                                        Speed = req.Speed
+                                                      },ct);
+        if (response.IsError)
+        {
+            foreach (var error in response.Errors)
+            {
+                AddError(error.Description, error.Code);
+            }
+
+            ThrowError("ErrorList");
+            return;
+        }
+
+        await SendNoContentAsync();
+    }
+}
---
+++ src/WebApi/v1/Users/UpdateUserLocation/UpdateUserLocationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.v1.Users.UpdateUserLocation;
+
+public class UpdateUserLocationRequest
+{
+    public Guid UserId { get; init; }
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
+    public double Accuracy { get; init; }
+    public double Heading { get; init; }
+    public double Speed { get; init; }
+}
