f4a47268fe6b7438a33cdc07e8502dec81219425
Paweł Nowak <666ponury@gmail.com>
2022-12-27 19:37:49 +0100

    Usunięcie metody do weryfikacji usera

--- src/WebApi/v1/Users/VerifyUser/VerifyUserRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MultiProject.Delivery.WebApi.v1.Users.VerifyUser;
-
-public sealed record VerifyUserRequest
-{
-    public string Username { get; init; } = default!;
-    public string Password { get; init; } = default!;
-}
