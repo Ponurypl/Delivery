fb31411e4bd4f562a3e43162f84d4ec93a6caa5d
Paweł Nowak <666ponury@gmail.com>
2023-01-19 20:51:53 +0100

    Dodanie pierwszego loga

---
+++ src/WebApi/Common/Logging/LogDefinitions.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiProject.Delivery.WebApi.Common.Logging;
+
+public partial class LogDefinitions
+{
+    [LoggerMessage(0, LogLevel.Warning, "Invalid logon attempt. Username used: {username}")]
+    public static partial void InvalidLogon(ILogger logger, string username);
+}
--- src/WebApi/v1/Auth/Login/LoginEndpoint.cs
+++ src/WebApi/v1/Auth/Login/LoginEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using MultiProject.Delivery.Application.Users.Queries.VerifyUser;
+using MultiProject.Delivery.WebApi.Common.Logging;
 using MultiProject.Delivery.WebApi.v1.Auth.Common;
 using MultiProject.Delivery.WebApi.v1.Auth.Services;
 
@@ -8,11 +9,13 @@ public sealed class LoginEndpoint : Endpoint<LoginRequest, LoginResponse>
 {
     private readonly ISender _sender;
     private readonly ITokenService _tokenService;
+    private readonly ILogger<LoginEndpoint> _logger;
 
-    public LoginEndpoint(ISender sender, ITokenService tokenService)
+    public LoginEndpoint(ISender sender, ITokenService tokenService, ILogger<LoginEndpoint> logger)
     {
         _sender = sender;
         _tokenService = tokenService;
+        _logger = logger;
     }
 
     public override void Configure()
@@ -26,14 +29,21 @@ public sealed class LoginEndpoint : Endpoint<LoginRequest, LoginResponse>
 
     public override async Task HandleAsync(LoginRequest req, CancellationToken ct)
     {
-        ErrorOr<VerifiedUserDto> result = await _sender.Send(new VerifyUserQuery() { Password = req.Password, Username = req.Username }, ct);
+        try
+        {
+            ErrorOr<VerifiedUserDto> result = await _sender.Send(new VerifyUserQuery() { Password = req.Password, Username = req.Username }, ct);
 
-        ValidationFailures.AddErrorsAndThrowIfNeeded(result);
+            ValidationFailures.AddErrorsAndThrowIfNeeded(result);
 
-        (TokenDetails Access, TokenDetails Refresh) tokens =
-            await _tokenService.GetNewTokensAsync(result.Value.Username, result.Value.Id, ct);
+            (TokenDetails Access, TokenDetails Refresh) tokens =
+                await _tokenService.GetNewTokensAsync(result.Value.Username, result.Value.Id, ct);
 
-        await SendOkAsync(new LoginResponse { AccessToken = tokens.Access, RefreshToken = tokens.Refresh, }, ct);
-
+            await SendOkAsync(new LoginResponse { AccessToken = tokens.Access, RefreshToken = tokens.Refresh, }, ct);
+        }
+        catch 
+        {
+            LogDefinitions.InvalidLogon(_logger, req.Username);
+            throw;
+        }
     }
 }
