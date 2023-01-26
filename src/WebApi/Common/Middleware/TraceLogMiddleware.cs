61731e26cccec5f460c4c824d8fbe38c7829ffc6
Łukasz Burchard <llukkassi@gmail.com>
2023-01-26 21:57:46 +0100

    Dodanie Serilog-a i wpięcie pierwszego TraceLogMiddleware
    zmiana w apsettings pod serilog

---
+++ src/WebApi/Common/Middleware/TraceLogMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+
+namespace MultiProject.Delivery.WebApi.Common.Middleware;
+
+public sealed class TraceLogMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<TraceLogMiddleware> _logger;
+
+    public TraceLogMiddleware(RequestDelegate next, ILogger<TraceLogMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        Stopwatch time = null;
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            time = Stopwatch.StartNew();
+        }
+        //before endpoint
+        // Call the next delegate/middleware in the pipeline.
+        await _next(context);
+        //after endpoint
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            time?.Stop();
+            _logger.LogTrace("request took: {time}ms", time?.ElapsedMilliseconds);
+        }
+    }
+}
+public static class RequestCultureMiddlewareExtensions
+{
+    public static IApplicationBuilder UseTraceLogging(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<TraceLogMiddleware>();
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -10,36 +10,54 @@ using MultiProject.Delivery.WebApi.Common.Auth;
 using MultiProject.Delivery.WebApi.Common.Swagger;
 using NSwag;
 using System.Text;
+using Serilog;
+using MultiProject.Delivery.WebApi.Common.Middleware;
+using Serilog.Events;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+      .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+      .Enrich.FromLogContext()
+      .WriteTo.File("logs/Delivery_.log", rollingInterval: RollingInterval.Day)
+      .CreateBootstrapLogger();
 
-var services = builder.Services;
-services.AddApplicationLayerServices();
-services.AddInfrastructureLayerServices(builder.Configuration);
-services.AddWebApiLayerServices();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .MinimumLevel.Verbose()
+    .Enrich.FromLogContext()
+    .WriteTo.File("logs/Delivery_.log", rollingInterval: RollingInterval.Day)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u4}] {SourceContext}: {Message:lj}{NewLine}{Exception}"));
 
-services.AddFastEndpoints();
-services.AddAuthentication(o =>
-                           {
-                               o.DefaultAuthenticateScheme = AuthConsts.AccessSchema;
-                               o.DefaultChallengeScheme = AuthConsts.AccessSchema;
-                           })
-        .AddJwtBearer(AuthConsts.AccessSchema, o =>
-                      {
-                          o.TokenValidationParameters = new TokenValidationParameters
-                                                         {
-                                                             ValidateIssuerSigningKey = true,
-                                                             ValidateAudience = true,
-                                                             ValidAudience = AuthConsts.AccessSchema,
-                                                             ValidateIssuer = false,
-                                                             ValidateLifetime = true,
-                                                             ClockSkew = TimeSpan.FromSeconds(60),
-                                                             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthConsts.JwtSigningKey))
-                                                         };
-                      })
-        .AddJwtBearer(AuthConsts.RefreshSchema, o =>
-                                               {
-                                                   o.TokenValidationParameters = new TokenValidationParameters
+    var services = builder.Services;
+
+    services.AddApplicationLayerServices();
+    services.AddInfrastructureLayerServices(builder.Configuration);
+    services.AddWebApiLayerServices();
+
+    services.AddFastEndpoints();
+    services.AddAuthentication(o =>
+                               {
+                                   o.DefaultAuthenticateScheme = AuthConsts.AccessSchema;
+                                   o.DefaultChallengeScheme = AuthConsts.AccessSchema;
+                               })
+            .AddJwtBearer(AuthConsts.AccessSchema, o =>
+                          {
+                              o.TokenValidationParameters = new TokenValidationParameters
+                              {
+                                  ValidateIssuerSigningKey = true,
+                                  ValidateAudience = true,
+                                  ValidAudience = AuthConsts.AccessSchema,
+                                  ValidateIssuer = false,
+                                  ValidateLifetime = true,
+                                  ClockSkew = TimeSpan.FromSeconds(60),
+                                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthConsts.JwtSigningKey))
+                              };
+                          })
+            .AddJwtBearer(AuthConsts.RefreshSchema, o =>
+                                                   {
+                                                       o.TokenValidationParameters = new TokenValidationParameters
                                                        {
                                                            ValidateIssuerSigningKey = true,
                                                            ValidateAudience = true,
@@ -49,47 +67,57 @@ services.AddAuthentication(o =>
                                                            ClockSkew = TimeSpan.FromSeconds(60),
                                                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(AuthConsts.JwtSigningKey))
                                                        };
-                                               });
+                                                   });
 
-services.AddSwaggerDoc(settings =>
-                       {
-                           settings.Title = "Delivery.WebApi";
-                           settings.Version = "v1";
-                           settings.AddAuth(AuthConsts.AccessSchema,
-                                            new()
-                                            {
-                                                Type = OpenApiSecuritySchemeType.Http,
-                                                Scheme = JwtBearerDefaults.AuthenticationScheme,
-                                                BearerFormat = "JWT",
-                                            });
-                           settings.AddAuth(AuthConsts.RefreshSchema,
-                                            new()
-                                            {
-                                                Type = OpenApiSecuritySchemeType.Http,
-                                                Scheme = JwtBearerDefaults.AuthenticationScheme,
-                                                BearerFormat = "JWT",
-                                            });
-                           settings.OperationProcessors.Add(new AddUnauthorizedResponseOperationProcessor());
-                       },
-                       js =>
-                       {
-                           js.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-                           js.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-                       },
-                       shortSchemaNames: true, maxEndpointVersion: 1, addJWTBearerAuth: false);
+    services.AddSwaggerDoc(settings =>
+                           {
+                               settings.Title = "Delivery.WebApi";
+                               settings.Version = "v1";
+                               settings.AddAuth(AuthConsts.AccessSchema,
+                                                new()
+                                                {
+                                                    Type = OpenApiSecuritySchemeType.Http,
+                                                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                                                    BearerFormat = "JWT",
+                                                });
+                               settings.AddAuth(AuthConsts.RefreshSchema,
+                                                new()
+                                                {
+                                                    Type = OpenApiSecuritySchemeType.Http,
+                                                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                                                    BearerFormat = "JWT",
+                                                });
+                               settings.OperationProcessors.Add(new AddUnauthorizedResponseOperationProcessor());
+                           },
+                           js =>
+                           {
+                               js.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                               js.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                           },
+                           shortSchemaNames: true, maxEndpointVersion: 1, addJWTBearerAuth: false);
 
-var app = builder.Build();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseFastEndpoints(c =>
-                     {
-                         c.Endpoints.ShortNames = true;
-                         c.Endpoints.RoutePrefix = "api";
+    var app = builder.Build();
+    app.UseTraceLogging();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseFastEndpoints(c =>
+                         {
+                             c.Endpoints.ShortNames = true;
+                             c.Endpoints.RoutePrefix = "api";
 
-                         c.Versioning.DefaultVersion = 1;
-                         c.Versioning.Prefix = "v";
-                         c.Versioning.PrependToRoute = true;
-                     }); 
-app.UseSwaggerGen();
+                             c.Versioning.DefaultVersion = 1;
+                             c.Versioning.Prefix = "v";
+                             c.Versioning.PrependToRoute = true;
+                         });
+    app.UseSwaggerGen();
 
-app.Run();
+    app.Run();
+}
+catch(Exception ex)
+{
+    Log.Fatal(ex, "Aplication startup fatal error");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
