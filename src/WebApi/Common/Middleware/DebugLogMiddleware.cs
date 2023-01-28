2805b430219f95c912374c1103afe4f7f37e9258
Łukasz Burchard <llukkassi@gmail.com>
2023-01-28 12:38:25 +0100

    Wydzielenie RequestCultureMiddlewareExtension do osbnego pliku
    z pytaniem o wybór poziomu logowania.

--- src/WebApi/Common/Middleware/DebugLogMiddleware.cs
+++ src/WebApi/Common/Middleware/DebugLogMiddleware.cs
@@ -2,12 +2,12 @@
 
 namespace MultiProject.Delivery.WebApi.Common.Middleware;
 
-public sealed class TraceLogMiddleware
+public sealed class DebugLogMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger<TraceLogMiddleware> _logger;
+    private readonly ILogger<DebugLogMiddleware> _logger;
 
-    public TraceLogMiddleware(RequestDelegate next, ILogger<TraceLogMiddleware> logger)
+    public DebugLogMiddleware(RequestDelegate next, ILogger<DebugLogMiddleware> logger)
     {
         _next = next;
         _logger = logger;
@@ -16,15 +16,17 @@ public sealed class TraceLogMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
         Stopwatch time = null;
-        if (_logger.IsEnabled(LogLevel.Trace))
+        if (_logger.IsEnabled(LogLevel.Debug))
         {
             time = Stopwatch.StartNew();
         }
+
         //before endpoint
         // Call the next delegate/middleware in the pipeline.
         await _next(context);
         //after endpoint
-        if (_logger.IsEnabled(LogLevel.Trace))
+
+        if (_logger.IsEnabled(LogLevel.Debug))
         {
             time?.Stop();
             _logger.LogTrace("request took: {time}ms", time?.ElapsedMilliseconds);
---
+++ src/WebApi/Common/Middleware/RequestCultureMiddlewareExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MultiProject.Delivery.WebApi.Common.Middleware;
+
+public static class RequestCultureMiddlewareExtensions
+{
+    //TODO: może to tutaj lub przy budowaniu aplikacji powinniśmy określić jaki poziom logowania trzeba użyć zamiast
+    //powoływać każdy i przy każdym request-cie sprawdzać jaki jest poziom i czy dany middleware ma co robić?
+    public static IApplicationBuilder UseTraceLogging(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<TraceLogMiddleware>();
+    }
+
+    public static IApplicationBuilder UseDebugLogging(
+        this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<DebugLogMiddleware>();
+    }
+
+
+}
