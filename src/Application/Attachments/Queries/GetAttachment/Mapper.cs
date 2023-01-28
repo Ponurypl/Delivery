229dd54c8626fe8743ca6fb7207d29573ff2e5b0
Łukasz Burchard <llukkassi@gmail.com>
2023-01-28 16:21:18 +0100

    Walka z mapsterem w GetAttachment
    i mikro refaktor

--- src/Application/Attachments/Queries/GetAttachment/Mapper.cs
+++ src/Application/Attachments/Queries/GetAttachment/Mapper.cs
@@ -10,8 +10,9 @@ public sealed class Mapper : IRegister
               .Map(d => d.Id, s => s.Id.Value)
               .Map(d => d.CreatorId, s => s.CreatorId.Value)
               .Map(d => d.TransportId, s => s.TransportId.Value)
-              .Map(d => d.ScanId, s => s.ScanId!.Value)
-              .Map(d => d.TransportUnitId, s => s.TransportUnitId!.Value)
-              .Map(d => d.Status, s => (int) s.Status);
+              //TODO: chuj nie wiem jak wyciągnąc to, mapster zawsze krzyczy że się nie da.
+              .Map(d => d.TransportUnitId, s => s.TransportUnitId.Value, s => s.TransportUnitId.HasValue)
+              .Map(d => d.ScanId, s => s.ScanId.Value, s => s.ScanId.HasValue)
+              .Map(d => d.Status, s => (int)s.Status);
     }
 }
