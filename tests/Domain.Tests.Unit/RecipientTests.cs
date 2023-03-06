a6ea5070db2b7e9f39e189f0b767cf2e7cbf1e6b
Łukasz Burchard <llukkassi@gmail.com>
2023-03-06 18:36:13 +0100

    Dodanie testów jednostkowych dla ValueType Recipient

---
+++ tests/Domain.Tests.Unit/RecipientTests.cs
@@ -0,0 +1,61 @@
+﻿using MultiProject.Delivery.Domain.Common;
+using MultiProject.Delivery.Domain.Deliveries.ValueTypes;
+using MultiProject.Delivery.Domain.Tests.Unit.Helpers;
+
+namespace MultiProject.Delivery.Domain.Tests.Unit;
+public class RecipientTests
+{
+    [Fact]
+    public void Create_WhenValidDataProvided_ThenValidObjectReturned()
+    {
+        //Arrange
+        string companyName = "companyName";
+        string country = "country";
+        string flatNumber = "flatNumber";
+        string lastName = "lastname";
+        string name = "name";
+        string phoneNumber = "phoneNumber";
+        string postCode = "postCode";
+        string street = "street";
+        string streetNumber = "streetNumber";
+        string town = "town";
+
+        //Act
+        ErrorOr<Recipient> result = Recipient.Create(companyName, country, flatNumber, lastName, name, phoneNumber, postCode, street, streetNumber, town);
+
+        //Assert
+        result.IsError.Should().BeFalse();
+
+        Recipient obj = result.Value;
+        obj.Should().NotBeNull();
+        obj.CompanyName.Should().Be(companyName);
+        obj.Country.Should().Be(country);
+        obj.FlatNumber.Should().Be(flatNumber);
+        obj.LastName.Should().Be(lastName);
+        obj.Name.Should().Be(name);
+        obj.PhoneNumber.Should().Be(phoneNumber);
+        obj.PostCode.Should().Be(postCode);
+        obj.Street.Should().Be(street);
+        obj.StreetNumber.Should().Be(streetNumber);
+        obj.Town.Should().Be(town);
+    }
+
+    [Theory]
+    [ClassData(typeof(RecipientCreateWrongData))]
+    public void Create_WhenInvalidDataProvided_ThenFailureReturned(string? companyName, string country, string? flatNumber, string? lastName,
+                                            string? name, string phoneNumber, string postCode, string? street,
+                                            string streetNumber, string town)
+    {
+        //Arrange
+
+        //Act
+        ErrorOr<Recipient> result = Recipient.Create(companyName, country, flatNumber, lastName, name, phoneNumber, postCode, street, streetNumber, town);
+
+        //Assert
+        result.IsError.Should().BeTrue();
+        result.FirstError.Type.Should().Be(ErrorType.Validation);
+        result.FirstError.Should().Be(DomainFailures.Deliveries.InvalidRecipient);
+        result.Value.Should().BeNull();
+    }
+
+}
