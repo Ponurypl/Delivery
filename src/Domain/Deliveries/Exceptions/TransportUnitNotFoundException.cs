1bec7b31942b19fc9e35662f6ca1ebaba4206346
Łukasz Burchard <llukkassi@gmail.com>
2023-03-14 18:35:05 +0100

    wyrzucenie nie używanych TransporUnitException

--- src/Domain/Deliveries/Exceptions/TransportUnitNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MultiProject.Delivery.Domain.Deliveries.Exceptions;
-
-public sealed class TransportUnitNotFoundException : Exception
-{
-    public TransportUnitNotFoundException(int id) 
-        : base($"Transport unit Id {id} not found.")
-    {
-    }
-}
