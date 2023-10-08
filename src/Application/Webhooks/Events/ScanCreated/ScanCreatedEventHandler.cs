215713091ea7f2e2b4aab23e67b387f13ff1092f
Łukasz Burchard <llukkassi@gmail.com>
2023-10-08 22:15:08 +0200

    Rozbudowa ScanCreatedEvent
    Dodanie typu eventu w komunikacie
    zagnieżdżenie danych w evencie
    dodanie pól do IEvent

--- src/Application/Webhooks/Events/ScanCreated/ScanCreatedEventHandler.cs
+++ src/Application/Webhooks/Events/ScanCreated/ScanCreatedEventHandler.cs
@@ -13,7 +13,7 @@ internal class ScanCreatedEventHandler : IEventHandler<ScanCreatedEvent>
 
     public async Task Handle(ScanCreatedEvent notification, CancellationToken cancellationToken)
     {
-        JsonContent message = JsonContent.Create(notification, MediaTypeHeaderValue.Parse("application/json"));
+        JsonContent message = JsonContent.Create(notification, MediaTypeHeaderValue.Parse("application/json")); //todo: przenieść do infrastruktury samą wysyłkę komunikatu
         Uri requestUri = new("https://webhook.site/6bdca34c-aae8-435d-beaf-0d6b882ddfcd");
         HttpResponseMessage response = await _client.PostAsync(requestUri, message, cancellationToken);
         string responseString = await response.Content.ReadAsStringAsync(cancellationToken);
